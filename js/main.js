0354b7fe6e3bb55b0b5bf45c3a267ee4a53710f3
Abhishek Modi <abhishek.modi95@gmail.com>
2016-04-28 18:37:06 -0500

    some more nonsense

---
+++ js/main.js
@@ -0,0 +1,21 @@
+function setbg(){
+    var d = new Date();
+    d = d.getHours();
+    if (d < 6){
+        
+    }
+    if (d < 13){
+        
+    }
+    if (d < 16){
+        
+    }
+    if (d < 20){
+        
+    }
+}
+
+function onload(){
+    console.log("Hello there")
+    setbg();
+}
